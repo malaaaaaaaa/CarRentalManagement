121eb2be12e35d222e234dfa04691b2a3ef95b5a
David Medina <fdavidbmedina@gmail,com>
2023-11-23 15:15:26 +0800

    Added database and default data

--- CarRentalManagement/Server/Data/ApplicationDbContext.cs
+++ CarRentalManagement/Server/Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
-﻿using CarRentalManagement.Server.Models;
+﻿using CarRentalManagement.Server.Configurations.Entities;
+using CarRentalManagement.Server.Models;
+using CarRentalManagement.Shared.Domain;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Drawing;
 
 namespace CarRentalManagement.Server.Data
 {
@@ -12,6 +15,25 @@ namespace CarRentalManagement.Server.Data
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Make> Makes { get; set; }  
+        public DbSet<Model> Models { get; set; }  
+        public DbSet<Customer> Customers { get; set; }  
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ColourSeedConfiguration());
+            builder.ApplyConfiguration(new MakeSeedConfiguration());
+            builder.ApplyConfiguration(new ModelSeedConfiguration());
+            builder.ApplyConfiguration(new RoleSeedConfiguration());
+            builder.ApplyConfiguration(new UserRoleSeedConfiguration());
+            builder.ApplyConfiguration(new UserSeedConfiguration());
         }
     }
 }
--- CarRentalManagement/Server/Models/ApplicationUser.cs
+++ CarRentalManagement/Server/Models/ApplicationUser.cs
@@ -4,5 +4,9 @@ namespace CarRentalManagement.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+
+
     }
 }
